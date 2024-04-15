fe96ccf1991cc2b1096673263c2cc724db489071
Maira Naeem <163671644+mairanoor412@users.noreply.github.com>
2024-04-15 08:26:10 -0700

    todo-list

---
+++ index.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let todoQuestions = await inquirer.prompt([
+        {
+            name: "firstQuestion",
+            type: "input",
+            message: "What would you like to add in you todos?",
+        },
+        {
+            name: "secondQuestion",
+            type: "confirm",
+            message: "Would you like to add more in your todos?",
+            default: "true",
+        }
+    ]);
+    todos.push(todoQuestions.firstQuestion);
+    console.log(todos);
+    condition = (todoQuestions.secondQuestion);
+}
